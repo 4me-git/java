fa4f74e0ad64b3a855ad55c0c9556eadfd88d4c4
rabbit <rabbit@localhost>
2025-09-22 10:06:54 +0900

    課題提出

---
+++ HalloWorld_FujimotoAkira/src/basicClass/Animal.java
@@ -0,0 +1,23 @@
+package basicClass;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Animal {
+	
+	public static void main(String[] args) {
+		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		Dog dog1 = new Dog(); //new Dog();でDogクラスのインスタンスを作成
+		System.out.println(dog1.name); //nameフィールドを参照
+		
+		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		Dog dog2 = new Dog(3); 
+		System.out.println(dog2.count);
+		
+		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+		LocalDateTime now = LocalDateTime.now(); //日時を取得
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyy-MM-dd H:m:s"); //フォーマット指定
+		String formattedNow = now.format(formatter); //文字列に変換 format(formatter)で指定
+		System.out.println(formattedNow);
+	}
+}
